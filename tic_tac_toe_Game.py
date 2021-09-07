b2dd6107bc673bd0a2cdbc00f68e385dba9dd1a8
Matrix77777777 <89752200+Matrix77777777@users.noreply.github.com>
2021-09-07 15:58:57 -0400

    Create tic_tac_toe_Game.py

---
+++ tic_tac_toe_Game.py
@@ -0,0 +1,25 @@
+def draw_playing_field(value_table, slip = 30):
+    '''
+    Draws a game board with the current cell values.
+    '''
+    pass
+
+def take_input(player_current):
+    '''
+    Checks for correct input and fills in cell values.
+    '''
+    pass
+
+def check_win(value_table):
+   pass
+
+
+value_table = list(range(1,10))
+player_current, player_next = 'O', 'X'
+counter = 0
+win = False
+print('*' * 10, ' TIC-TAC-TOE GAME for two players ', '*' * 10)
+
+
+
+input('Press Enter to exit!')
